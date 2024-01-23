7eb51ed7510342f934655d95c51e723b4845dd8b
NomelN <akpanomel@gmail.com>
2024-01-23 11:33:51 +0100

    update

--- Application/views.py
+++ Application/views.py
@@ -4,7 +4,7 @@ import datetime
 
 # Create your views here.
 def index(request):
-    API_KEY = open("C:\\Users\\akpan\\Desktop\\Projets_personnels\\MeteoApp\\API_KEY", "r").read()
+    API_KEY = "your api key"
     current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
     forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
 
